60c754b63fa8af08c8aafb69d2cfea321fa77044
WcjMBP <736865160@qq.com>
2016-08-11 14:42:08 +0800

    self.windows = 1

--- ViewController.m
+++ ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"");
     self.view.backGroundColor = [UIColor redColor];
+    self.windows = 1;
     
 }
 
